84ad5cc857f295328ebd7cd991fc0d06561ec7dc
Albert <albertwilliams.xyz@gmail.com>
2024-12-22 15:39:41 -0400

    Adding mapping to jump to differnt parts of a very snippet

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,5 @@
 return {
         "williamboman/mason-lspconfig.nvim",
         lazy = false,
-        opts = {
-                ensure_install = { "lua_ls", "stylua" },
-                auto_install = true,
-        },
+        opts = { auto_install = true },
 }
