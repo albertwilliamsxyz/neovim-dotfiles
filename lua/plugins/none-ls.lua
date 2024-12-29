88739a4b98e245ca6e90381af99ea7f242af4d09
Albert <albertwilliams.xyz@gmail.com>
2024-12-29 18:26:48 -0400

    Temporarily remove some sources from none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,8 +7,6 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.completion.spell,
-				require("none-ls.diagnostics.eslint"),
 			},
 		})
 	end,
